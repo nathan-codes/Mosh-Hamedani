d1ec990bae18eafa3b9dff7f9986082abf411807
Nathan Turkson <nathanturkson@gmail.com>
2024-08-27 10:47:37 +0000

    handle Games with no image

--- game-Hub/src/services/getCroppedImageUrl.ts
+++ game-Hub/src/services/getCroppedImageUrl.ts
@@ -1,6 +1,7 @@
+import defaultImage from "../assets/no-image-placeholder-6f3882e0.webp"
 const getCroppedImages = (url: string) => {
   const target = "media/";
-  if (!url) return ""
+  if (!url) return defaultImage;
   const insertPlace = url.indexOf(target) + target.length;
   return url.slice(0, insertPlace) + "crop/600/400/" + url.slice(insertPlace);
 };
